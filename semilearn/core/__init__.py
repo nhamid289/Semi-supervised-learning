974dcbf2371d7b7293c4e2ee5dfd4fb4198136eb
Nazeef Hamid <nazeef.h.289@gmail.com>
2025-04-01 13:34:45 +1000

    fix: fixes outdated import

--- semilearn/core/__init__.py
+++ semilearn/core/__init__.py
@@ -5,6 +5,4 @@
 from .algorithmbase import AlgorithmBase, ImbAlgorithmBase
 from .utils.registry import import_all_modules_for_register
 
-from .ssl_algorithm import SSLAlgorithm
-
 import_all_modules_for_register()
