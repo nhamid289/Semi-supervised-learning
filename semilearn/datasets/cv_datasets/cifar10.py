69acbea1493fdcb12c700452bb6855c390bd04ee
Nazeef Hamid <nazeef.h.289@gmail.com>
2025-04-11 09:03:37 +1000

    feat: adds transforms and dataset loading to Cifar10 dataset class

--- semilearn/datasets/cv_datasets/cifar10.py
+++ semilearn/datasets/cv_datasets/cifar10.py
@@ -3,11 +3,17 @@ from torchvision import transforms
 
 from semilearn.datasets import SSLDataset
 from semilearn.utils.data import BaseDataset, split_lb_ulb_balanced
+from semilearn.datasets.augmentation import RandAugment
 
 class Cifar10(SSLDataset):
 
-    def __init__(self):
-        pass
+    def __init__(self, num_lbl=4, num_ulbl=None, seed=None,
+                 crop_size=32, crop_ratio=1,
+                 data_dir = "~/SSLDatasets/CIFAR10", download=True):
+
+        self._define_transforms(crop_size, crop_ratio)
+
+        self._get_dataset(num_lbl, num_ulbl, seed, data_dir, download)
 
     def _define_transforms(self, crop_size, crop_ratio):
 
@@ -45,6 +51,28 @@ class Cifar10(SSLDataset):
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
 
+    def _get_dataset(self, num_lbl, num_ulbl, seed, data_dir, download):
+
+        cifar10_tr = CIFAR10(data_dir, train=True, download=download)
+        X, y = cifar10_tr.data, cifar10_tr.targets
+        X_lb, y_lb, X_ulb, y_ulb = split_lb_ulb_balanced(X, y, num_lbl=num_lbl,
+                                                         num_ulbl=num_ulbl,
+                                                         seed=seed)
+
+        self.lbl_dataset = BaseDataset(X=X_lb, y=y_lb, num_classes=10,
+                                       weak_transform=self.weak_transform,
+                                       medium_transform=self.strong_transform,
+                                       strong_transform=self.strong_transform)
+        self.ulbl_dataset = BaseDataset(X=X_ulb, y=y_ulb, num_classes=10,
+                                       weak_transform=self.weak_transform,
+                                       medium_transform=self.medium_transform,
+                                       strong_transform=self.strong_transform)
+
+        cifar10_test = CIFAR10(data_dir, train=False, download=download)
+
+        X, y = cifar10_test.data, cifar10_test.targets
+        self.eval_dataset = BaseDataset(X=X, y=y, num_classes=10,
+                                        weak_transform=self.eval_transform)
 
 # class Cifar10(SSLDatasetInterface):
 
