be066db97476da01fc387ca16d29cf4cfcc5df56
Nazeef Hamid <nazeef.h.289@gmail.com>
2025-04-23 16:14:11 +1000

    feat: adds new SSLCyclicLoader class, converts SSLDataLoader to abstract class and adds previous implementation as SSLMinimumLoader

--- semilearn/utils/data/dataloader.py
+++ semilearn/utils/data/dataloader.py
@@ -25,6 +25,26 @@ class SSLBatch():
         return self
 
 class SSLDataLoader(DataLoader):
+    """
+    An SSL Dataloader provides batches which contain both labelled and
+    unlabelled data.
+    """
+
+
+    def __next__(self):
+        """
+        Return the next labelled and unlabelled batch.
+
+        The return from __next__ should be an SSLBatch object or a subclass of
+        SSLBatch
+        """
+        return NotImplementedError
+
+class SSLMinimumLoader(SSLDataLoader):
+    """
+    A dataloader which terminates after either the labelled or unlabelled
+    has been exhausted.
+    """
 
     def __init__(self, lbl_dataset, ulbl_dataset,
                  lbl_batch_size=1, ulbl_batch_size=1,
@@ -50,5 +70,45 @@ class SSLDataLoader(DataLoader):
         return SSLBatch(*lbl_batch, *ulbl_batch)
 
     def __len__(self):
-        # return len(self.ulbl_loader)
         return min(len(self.lbl_loader), len(self.ulbl_loader))
+
+class SSLCylicLoader(SSLDataLoader):
+    """
+    A dataloader that continuously provides labelled and unlabelled batches.
+    If either the labelled or unlabelled data is exhausted, it is reshuffled
+    and the batches continue to be loaded.
+    """
+
+    def __init__(self, lbl_dataset, ulbl_dataset,
+                 lbl_batch_size=1, ulbl_batch_size=1,
+                 shuffle_lbl=True, shuffle_ulbl=True,
+                 num_workers=0):
+
+        self.lbl_dataset = lbl_dataset
+        self.ulbl_dataset = ulbl_dataset
+
+        self.lbl_loader = DataLoader(lbl_dataset, batch_size=lbl_batch_size,
+                                     shuffle=shuffle_lbl, num_workers=num_workers)
+        self.ulbl_loader = DataLoader(ulbl_dataset, batch_size=ulbl_batch_size,
+                                      shuffle=shuffle_ulbl, num_workers=num_workers)
+
+        self.lbl_iter = iter(self.lbl_loader)
+        self.ulbl_iter = iter(self.ulbl_loader)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            lbl_batch = next(self.lbl_iter)
+        except StopIteration:
+            self.lbl_iter = iter(self.lbl_loader)
+            lbl_batch = next(self.lbl_iter)
+
+        try:
+            ulbl_batch = next(self.ulbl_iter)
+        except StopIteration:
+            self.ulbl_iter = iter(self.loader)
+            ulbl_batch = next(self.ulbl_iter)
+
+        return SSLBatch(*lbl_batch, *ulbl_batch)
